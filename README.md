b8592a688aaf84b8bd9257af8f7c26e8c1f5fc78
zjipsen <zjipsen@gmail.com>
2024-02-05 14:12:49 -0800

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # mastermind
 play the game mastermind with command line visualization.
+
+Run tests:
+
+	> python3 tests/tests.py 
+
+Begin game:
+
+	> (not ready yet) 
--- tests/tests.py
+++ tests/tests.py
@@ -2,15 +2,14 @@ from context import mastermind
 from mastermind import mastermind
 import sys, unittest
 
-def test_code():
-    c1 = mastermind.Code([])
-    print(f'{c1}')
-    print("hi")
+class TestCodeMethods(unittest.TestCase):
 
+    def test_instantiate(self):
+        self.assertIsInstance(mastermind.Code([]), mastermind.Code)
 
 def main() -> int:
     """Echo the input arguments to standard output"""
-    test_code()
+    unittest.main()
 
 if __name__ == '__main__':
     sys.exit(main())
