a57f38d962806e11fdf5c54c0087d389160ffed2
zjipsen <zjipsen@gmail.com>
2024-02-12 15:04:27 -0800

    Added colors to prompt, began game logic

--- mastermind/mastermind.py
+++ mastermind/mastermind.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from enum import Enum
-from prompt_toolkit import prompt
+from prompt_toolkit import HTML, print_formatted_text, prompt
 from typing import List
 
 import click
@@ -47,8 +47,18 @@ class Code:
 
 
 class Mastermind:
-    def __init__(self, answer_code: Code):
+    def __init__(self, answer_code: Code=None):
         self.answer_code = answer_code
+
+    def generate_random_answer_code(self):
+        pass
+
+    def is_valid_guess(self, guess) -> bool:
+        pass
+
+    def make_guess(self, guess) -> List[Feedback]:
+        pass
+
 
     """
     MOST BASIC VERSION: what needs to happen every new game?
@@ -69,27 +79,65 @@ class Mastermind:
     """
 
 
+def skyblue(text):
+    return f'<skyblue>{text}</skyblue>'
 
+def blue(text):
+    return f'<deepskyblue>{text}</deepskyblue>'
 
+def seagreen(text):
+    return f'<seagreen>{text}</seagreen>'
 
+def violet(text):
+    return f'<violet>{text}</violet>'
 
+def white(text):
+    return f'<white>{text}</white>'
 
+def yellow(text):
+    return f'<gold>{text}</gold>'
 
-
-
-
-
-
-
+def red(text):
+    return f'<indianred>{text}</indianred>'
 
 
 @click.command("hello")
 @click.version_option("0.1.0", prog_name="mastermind")  # TODO: replace version & name with setup.py version/name
 def play():
     click.echo("Welcome to Mastermind! (exit with control-D)")
+    num_total_colors = 6
+    num_colors = 4
+    num_turns = 11
+
+    # loop until user exits with control-D
     while 1:
+        print_formatted_text(HTML(skyblue('Would you like to start a new game? (y/n)')))
         user_input = prompt('>')
-        print(user_input)
+
+        if user_input.lower() == "y":
+            print_formatted_text(HTML(skyblue(f"Generating code using {num_colors} out of {num_total_colors} colors.")))
+            print_formatted_text(HTML(skyblue(f"Here are the possible colors: ")))
+            print_formatted_text(HTML(white("white, ") + blue("blue, ") + seagreen("green, ") + yellow("yellow, ") + red("red, ") + violet("violet.")))
+
+            print_formatted_text(HTML(skyblue(f"\nPlease make your first guess by listing four colors (repeats okay), separated by spaces.")))
+
+            playing_game = True
+            while playing_game:
+                game = Mastermind()
+
+                user_input = prompt('>')
+                if game.is_valid_guess(user_input):
+                    feedback = game.make_guess(user_input)
+                else:
+                    pass # re-get user input until it's valid
+
+
+        if user_input == "n":
+            print_formatted_text(HTML(violet("Well. I'm not sure how to help you then. Are you having a nice day?")))
+            user_input = prompt('>')
+            print_formatted_text("++" + user_input)
+
+
 
 def main() -> int:
     """Echo the input arguments to standard output"""
